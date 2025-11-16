fcec657d04104c48d29c5de89a2b875af6cb3721
Arpit Jorelia <ar370282@dal.ca>
2025-11-16 17:36:17 -0400

    test

---
+++ labs/lab5/my-app/src/App.js
@@ -0,0 +1,40 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="container text-center">
+      <h1>Welcome to My Full-Stack App </h1>
+      <p>This is a React app styled with Bootstrap 5.</p>
+    </div>
+  );
+}
+
+export default App;
